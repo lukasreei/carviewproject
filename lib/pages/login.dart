3c8e7d8475353c0181ec1812ddd5ecc4dbe6ea65
Lukas <lukas581800@gmail.com>
2024-11-12 21:37:13 -0300

    atualização da page login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:carviewproject/pages/authentication/google.dart';
 import 'homepage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   final GoogleAuthService _googleAuthService = GoogleAuthService();
@@ -11,52 +12,77 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login'),titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
-      backgroundColor: Colors.black87,),
-      backgroundColor: Colors.grey,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                final user = await _googleAuthService.signInWithGoogle();
-                if (user != null) {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Homepage(analytics: FirebaseAnalytics.instance),
-                    ),
-                  );
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Falha ao fazer login, tente novamente.')),
-                  );
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.black87, backgroundColor: Colors.amberAccent,
-                minimumSize: Size(160, 60),
-              ),
-              child: const Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.login, size: 24, color: Colors.black87,),
-                  SizedBox(width: 12,),
-                  Text('Login com Google',
-                      style: TextStyle(fontSize: 24),
-                  ),
-                ],
-              ),
+      appBar: AppBar(
+        title: const Text('Login'),
+        titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
+        backgroundColor: Colors.black87,
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(image: AssetImage('assets/images/login.png'),fit: BoxFit.cover),
             ),
-            const SizedBox(height: 20,),
-            ElevatedButton(onPressed: (){},
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(160, 60)
-              ),
-            child: Text('Register', style: TextStyle(fontSize: 24),),)
-          ],
-        ),
+          ),
+          const Positioned(
+            top: 100,
+            left: 0,
+            right: 0,
+            child: Text('CARS STORE',
+              textAlign: TextAlign.center,
+              style:TextStyle(fontSize: 38, fontWeight: FontWeight.bold, color: Colors.red),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    final user = await _googleAuthService.signInWithGoogle();
+                    if (user != null) {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Homepage(analytics: FirebaseAnalytics.instance),
+                        ),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Falha ao fazer login, tente novamente.')),
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black87,
+                    backgroundColor: Colors.red,
+                    minimumSize: Size(160, 60),
+                  ),
+                  child: const Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.login, size: 24, color: Colors.black87),
+                      SizedBox(width: 12),
+                      Text(
+                        'Login com Google',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(minimumSize: Size(160, 60)),
+                  child: Text(
+                    'Register',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
